060e4787ea059003d17b127aa66fb94a4a4289d5
金铭 <14517833+jin-ming-05@user.noreply.gitee.com>
2026-07-28 22:32:36 +0800

    feat(renderer): style analysis components

---
+++ apps/desktop/src/renderer/components/code-analysis/index.ts
@@ -0,0 +1,8 @@
+export { AnalysisMarkdownViewer } from './AnalysisMarkdownViewer';
+export { AnalysisPromptBox } from './AnalysisPromptBox';
+export { AnnotationSidebar } from './AnnotationSidebar';
+export { ExportMenu } from './ExportMenu';
+export { ProjectSidebar } from './ProjectSidebar';
+export { ToolTraceTimeline } from './ToolTraceTimeline';
+export type { AnalysisAnnotationItem } from './AnnotationSidebar';
+export type { ToolTraceItem } from './ToolTraceTimeline';
